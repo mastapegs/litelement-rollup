d10eb550a1763cd216f6c6cfe3123afe57170c2d
Matthew Pagan <mastapegs01@gmail.com>
2020-12-29 11:51:52 -0500

    sites coming together

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-import { LitElement, html, customElement, property } from "lit-element";
+import { LitElement, html, css, customElement, property } from "lit-element";
+import "./components/TopBar";
 
 @customElement("app-component")
 export class AppComponent extends LitElement {
+  
+  static get styles() {
+    return css`
+      .container {
+        width: 80%;
+        margin: 0 auto;
+      }
+    `
+  }
+  
   @property() message = "Hello, Rollup!!!";
+
   render() {
     return html`
-      <h1>${this.message}</h1>
-      <p>LitElement with Rollup and Typescript</p>
+      <top-bar></top-bar>
+      <div class="container">
+        <h1>${this.message}</h1>
+        <p>LitElement with Rollup and Typescript</p>
+      </div>
     `;
   }
+  
 }
